5458d824e9c6ac0dfbc227eb7bedf0bc19c78467
Paulo Dias <paulo.dias@algonomy.com>
2023-05-26 16:25:50 -0300

    recs

--- Pages/epoca/rr.js
+++ Pages/epoca/rr.js
@@ -11,6 +11,7 @@ function configureR3() {
     // Configurações básicas 
     window.R3_COMMON = new r3_common();
     window.R3_COMMON.setApiKey('c85912f892c73e30'); // Chave de identificação da loja (Solicitar ao Consultor)
+    window.R3_COMMON.apiClientKey = 'd87e7a0748a78f10'; 
     window.R3_COMMON.setBaseUrl('https://' + rr_env + '.richrelevance.com/rrserver/'); // Define endpoint
     window.R3_COMMON.setClickthruServer(window.location.protocol + '//' + window.location.host); // Define a base de click dos produtos
     window.R3_COMMON.setSessionId('rr-345g67-63gyt7-376585-hdjf-374ee8'); // Sessão única em toda a navegação, deve ser alterada APÓS o envio do pedido finalizado
