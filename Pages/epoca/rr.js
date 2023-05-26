7bc2701164910bb8463ef273b270cac4ef6887ad
Paulo Dias <paulo.dias@algonomy.com>
2023-05-26 18:17:13 -0300

    commit

--- Pages/epoca/rr.js
+++ Pages/epoca/rr.js
@@ -1,5 +1,5 @@
 function configureR3() {
-    let rr_env = "recs"; // Definido para Ambiente de integração: integration = homologação - recs = produção
+    let rr_env = "integration"; // Definido para Ambiente de integração: integration = homologação - recs = produção
 
     // A condicional abaixo permite alterar o ambiente das recomendações passando um parâmetro na URL. Exemplo: https://demo.richrelevance.com/?r3_env=integration
     if (window.location.href.indexOf("r3_env=integration") > -1) {
@@ -11,7 +11,7 @@ function configureR3() {
     // Configurações básicas 
     window.R3_COMMON = new r3_common();
     window.R3_COMMON.setApiKey('c85912f892c73e30'); // Chave de identificação da loja (Solicitar ao Consultor)
-    window.R3_COMMON.apiClientKey = 'd87e7a0748a78f10'; 
+    window.R3_COMMON.apiClientKey = 'd87e7a0748a78f10';
     window.R3_COMMON.setBaseUrl('https://' + rr_env + '.richrelevance.com/rrserver/'); // Define endpoint
     window.R3_COMMON.setClickthruServer(window.location.protocol + '//' + window.location.host); // Define a base de click dos produtos
     window.R3_COMMON.setSessionId('rr-345g67-63gyt7-376585-hdjf-374ee8'); // Sessão única em toda a navegação, deve ser alterada APÓS o envio do pedido finalizado
